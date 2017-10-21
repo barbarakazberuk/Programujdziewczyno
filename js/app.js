c5a291e3c3651ff37dd11766e502c661fbc86b90
Basia Kazberuk <barbara.kazberuk@onet.pl>
2017-10-21 15:58:56 +0200

    task2javascript

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("działa!");
+var myName = 'Basia';
+console.log('myName:', myName);
+var friends = ["Agnieszka", "Ania", "Asia"];
+console.log(friends[1]);
+friends[0]="Gosia";
+console.log(friends);
+var a=2;
+var b=1;
+if(a>b){
+   console.log("hurra!")
+   };
+for (var i=0; i<=friends.length-1;i++){
+    console.log(friends[i]);
+}
+
+
+
